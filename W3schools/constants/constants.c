8dde312d94907dedfe1b0e0379ab697b3c0897c7
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-12-31 19:24:34 -0700

    constants

---
+++ W3schools/constants/constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // myNum will always be 15; an attempt to change it will result in an error.
+  const int myNum = 15;
+  //myNum = 12;
+
+  // Constants must be declared with a value
+
+  //Conventional to put constants into ALL_CAPS_SNAKE_CASE
+
+  const int BIRTH_YEAR = 1989;
+
+}
